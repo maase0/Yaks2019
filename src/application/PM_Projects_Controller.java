05a90aa8795c7abcfd83251941c6c4752a69b7e0
mjewell24 <mjewell24@users.noreply.github.com>
2019-10-09 18:17:05 -0400

    Created basic Product Manager - Projects page and linked login page to it

---
+++ src/application/PM_Projects_Controller.java
@@ -0,0 +1,40 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+
+public class PM_Projects_Controller implements Initializable{
+
+	@FXML
+	private Button newProjectBtn;
+	
+	@FXML
+	private CheckBox submittedTab;
+	
+	@FXML
+	private CheckBox unsubmittedTab;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+		
+	}
+	
+	public void selectUnsubmitted(ActionEvent event) {
+		
+	}
+	
+	public void selectSubmitted(ActionEvent event) {
+		
+	}
+	
+	public void addNewProject(ActionEvent event) {
+		
+	}
+}

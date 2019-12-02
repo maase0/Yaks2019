dde70c198dea73fcb6ddec6244cdf52cc2e6c613
rlevari <levari82@students.rowan.edu>
2019-12-02 17:50:28 -0500

    removed version from top-layer, CLIN_Estimate

--- src/application/CLIN_Estimate.java
+++ src/application/CLIN_Estimate.java
@@ -4,25 +4,14 @@ import java.util.ArrayList;
 
 public class CLIN_Estimate
 {
-    private String version;
     private ArrayList<OrganizationBOE> organizationBOEs;
 
     public CLIN_Estimate () {
-        version = null;
         organizationBOEs = null;
     }
 
-    public CLIN_Estimate (String version, ArrayList<OrganizationBOE> organizationBOEs) {
-        this.version = version;
+    public CLIN_Estimate (ArrayList<OrganizationBOE> organizationBOEs) {
         this.organizationBOEs = organizationBOEs;
-    }
-
-    public void setVersion(String version) {
-        this.version = version;
-    }
-
-    public String getVersion() {
-        return version;
     }
 
     public ArrayList<OrganizationBOE> getOrganizationBOEs() {

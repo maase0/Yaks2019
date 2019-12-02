bc6f8468a6065c2978cd10a72a2687cdae6f0a3e
rlevari <levari82@students.rowan.edu>
2019-12-02 17:22:16 -0500

    created objects for Tasks and Work Packages

--- src/application/Task.java
+++ src/application/Task.java
@@ -1,28 +1,108 @@
 package application;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+public class Task
+{
+    private String name;
+    private String popStart;
+    private String popEnd;
+    private String formula;
+    private int staffHours;
+    private String version;
+    private String details;
+    private String conditions;
+    private String methodology;
 
-public class Task extends Application {
-    @Override
-    public void start(Stage primaryStage) {
-        try {
-            // Read file fxml and draw interface.
-            Parent root = FXMLLoader.load(getClass()
-                    .getResource("Task.fxml"));
-
-            primaryStage.setTitle("Estimation Suite - Estimator - Projects Page");
-            primaryStage.setScene(new Scene(root));
-            primaryStage.show();
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
+    public Task() {
+        name = null;
+        popStart = null;
+        popEnd = null;
+        formula = null;
+        staffHours = 0;
+        version = null;
+        details = null;
+        conditions = null;
+        methodology = null;
     }
 
-    public static void main(String[] args) {
-        launch(args);
+    public Task(String name, String popStart, String popEnd, String formula,
+                int staffHours, String version, String details, String conditions,
+                String methodology) {
+        this.name = name;
+        this.popStart = popStart;
+        this.popEnd = popEnd;
+        this.formula = formula;
+        this.staffHours = staffHours;
+        this.version = version;
+        this.details = details;
+        this.conditions = conditions;
+        this.methodology = methodology;
+    }
+
+    public void setName (String name) {
+        this.name = name;
+    }
+
+    public void setPopStart (String popStart) {
+        this.popStart = popStart;
+    }
+
+    public void setPopEnd (String popEnd) {
+        this.popEnd = popEnd;
+    }
+
+    public void setFormula (String formula) {
+        this.formula = formula;
+    }
+
+    public void setStaffHours (int staffHours) {
+        this.staffHours = staffHours;
+    }
+
+    public void setVersion (String version) {
+        this.version = version;
+    }
+
+    public void setDetails (String details) {
+        this.details = details;
+    }
+
+    public void setConditions (String conditions) {
+        this.conditions = conditions;
+    }
+
+    public void setMethodology (String methodology) {
+        this.methodology = methodology;
+    }
+
+    public String getName () {
+        return name;
+    }
+
+    public String getPopStart () {
+        return popStart;
+    }
+
+    public String getPopEnd () {
+        return popEnd;
+    }
+
+    public String getFormula () {
+        return formula;
+    }
+
+    public String getVersion () {
+        return version;
+    }
+
+    public String getDetails () {
+        return details;
+    }
+
+    public String getConditions () {
+        return conditions;
+    }
+
+    public String getMethodology () {
+        return methodology;
     }
 }
--- src/application/WorkPackage.java
+++ src/application/WorkPackage.java
@@ -1,28 +1,112 @@
 package application;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import java.util.ArrayList;
 
-public class WorkPackage extends Application {
-    @Override
-    public void start(Stage primaryStage) {
-        try {
-            // Read file fxml and draw interface.
-            Parent root = FXMLLoader.load(getClass()
-                    .getResource("WorkPackage.fxml"));
+public class WorkPackage {
+    private String name;
+    private String wptype;
+    private String author;
+    private String popStart;
+    private String popEnd;
+    private String scope;
+    private String worktype;
+    private String version;
+    private ArrayList<Task> tasks = new ArrayList();
+    private ArrayList<Task> taskDelete = new ArrayList<Task>();
 
-            primaryStage.setTitle("Estimation Suite - Estimator - Projects Page");
-            primaryStage.setScene(new Scene(root));
-            primaryStage.show();
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
+    public WorkPackage() {
+        name = null;
+        wptype = null;
+        author = null;
+        popStart = null;
+        popEnd = null;
+        scope = null;
+        worktype = null;
+        version = null;
     }
 
-    public static void main(String[] args) {
-        launch(args);
+    public WorkPackage(String name, String wptype, String author,
+                       String popStart, String popEnd, String scope,
+                       String worktype, String version) {
+        this.name = name;
+        this.wptype = wptype;
+        this.author = author;
+        this.popStart = popStart;
+        this.popEnd = popEnd;
+        this.scope = scope;
+        this.worktype = worktype;
+        this.version = version;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setWptype(String wptype) {
+        this.wptype = wptype;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setPopStart(String popStart) {
+        this.popStart = popStart;
+    }
+
+    public void setPopEnd(String popEnd) {
+        this.popEnd = popEnd;
+    }
+
+    public void setScope(String scope) {
+        this.scope = scope;
+    }
+
+    public void setWorktype(String worktype) {
+        this.worktype = worktype;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getWptype() {
+        return wptype;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getPopStart() {
+        return popStart;
+    }
+
+    public String getPopEnd() {
+        return popEnd;
+    }
+
+    public String getScope() {
+        return scope;
+    }
+
+    public String getWorktype() {
+        return worktype;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public ArrayList<Task> getTasks() {
+        return tasks;
+    }
+
+    public void removeTask (Task task) {
+        this.tasks.remove(task);
     }
 }
